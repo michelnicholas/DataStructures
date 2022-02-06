7e4b6aeb12bac409ea2019e6cc7279e6e33e56bc
Michel Nicholas <michelnicholas1993@gmail.com>
2022-02-06 13:19:04 -0500

    First commit ArrayListDemo

---
+++ src/main/java/ArrayListDemo.java
@@ -0,0 +1,33 @@
+public class ArrayListDemo {
+
+    /*
+    What is an Array List?
+    ArrayList is an implementation of java.util.List interface.
+    ArrayList is back by an Array.
+
+    How ArrayList is different from Array?
+    ArrayList:
+    ArrayList size grows dynamically when we add more and more elements.
+    ArrayList provides various methods to work with the elements.
+
+    Array:
+    Array is fixed size but compartively faster.
+    Array can be multi-dimensional like 2D and 3D arrays.
+
+    Implement the method getUnAssignedCodes() in the following code.
+    Such that, unAssignedCodes = allCodes - assignedCodes
+     */
+
+    public static void main(String[] args) {
+        String[] allcodes = {"001","002","003","004","005","006","007","008","009"};
+        String[] assignedCodes = {"003","004"};
+
+        String[] unAssignedCodes = getUnAssignedCodes(allcodes,assignedCodes);
+
+    }
+
+    static String[] getUnAssignedCodes(String[] allcodes, String[] assignedCodes){
+        return null;
+    }
+
+}
