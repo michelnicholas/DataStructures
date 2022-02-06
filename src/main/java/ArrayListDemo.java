d56d660b1ea27e3055855462fe348159ac7ad12b
Michel Nicholas <michelnicholas1993@gmail.com>
2022-02-06 13:31:19 -0500

    added solution to arrayListDemo

--- src/main/java/ArrayListDemo.java
+++ src/main/java/ArrayListDemo.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayListDemo {
 
     /*
@@ -19,15 +23,32 @@ public class ArrayListDemo {
      */
 
     public static void main(String[] args) {
+        // Data
         String[] allcodes = {"001","002","003","004","005","006","007","008","009"};
+        // Dynamic Data
         String[] assignedCodes = {"003","004"};
-
+        // New String array  unAssignedCodes = allCodes - assignedCodes
         String[] unAssignedCodes = getUnAssignedCodes(allcodes,assignedCodes);
+        //Printed out new array
+        System.out.println(Arrays.toString(unAssignedCodes));
+
 
     }
 
+    // Created GetUnassigned Function to parse in our two arrays in the parameters
     static String[] getUnAssignedCodes(String[] allcodes, String[] assignedCodes){
-        return null;
+        // Used List <> to create new Array list and parsed in the string array allCodes to Arrays class with function
+        // as list
+        List<String> list = new ArrayList(Arrays.asList(allcodes));
+        // Used List <> to create new Array list and parsed in the string array assignedCodes to Arrays class with function
+        // as list
+        List<String> list2 = new ArrayList(Arrays.asList(assignedCodes));
+        // used list object to get methods inside and used removeAll from list2
+        list.removeAll(list2);
+        // returned the proper array size with a new string array without list 2
+        return list.toArray(new String[list.size()]);
+
+
     }
 
 }
